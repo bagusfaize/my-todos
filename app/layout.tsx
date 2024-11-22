0b70380c76ae182e654329937d60c7558f9483fb
bagusfaize <bagusfaize@gmail.com>
2024-11-22 21:31:29 +0700

    chore: setup react query

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Inter } from 'next/font/google'
+import "./globals.css";
+import ReactQueryProvider from "./providers/ReactQueryProvider";
+
+
+export const metadata: Metadata = {
+  title: "Simple To-Do App",
+  description: "Simple todo app",
+};
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>
+        <ReactQueryProvider>
+            {children}
+        </ReactQueryProvider>
+      </body>
+    </html>
+  );
+}
