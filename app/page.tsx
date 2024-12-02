c07b9a7e283f1c05cbba43272e2a7ba998d5ab0b
bagusfaize <bagusfaize@gmail.com>
2024-12-02 16:52:55 +0700

    refactor: refactor todo usequery

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
+'use client';
+
+import { useQuery } from "@tanstack/react-query";
 import TodoList from "./components/TodoList";
+import { getTodos } from "./services/todos";
 
 export default function Home() {
+  const { data: todos = [], isLoading } = useQuery({ queryKey: ['todos'], queryFn: getTodos });
+
   return (
     <div className="flex min-h-screen justify-center p-5 pb-20 gap-16 bg-slate-100">
       <main className="lg:w-1/2 flex flex-col gap-3">
-        <TodoList />
+        <TodoList
+          todos={todos}
+          isLoading={isLoading}
+        />
       </main>
     </div>
   );
